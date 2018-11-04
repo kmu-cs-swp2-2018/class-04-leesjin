1497c87fb4cd769f072d37568b09bb96d5225ade
leesjin <leesjin@kookmin.ac.kr>
2018-11-05 00:08:38 +0900

    두번째 guess

--- guess.py
+++ guess.py
@@ -5,22 +5,22 @@ class Guess: # model 이 관리
         self.guessedChars = [] #pdf 가 시켯서
         self.numTries = 0 # pdf 가 시켯서
         self.currentStatus = '' # pdf가 시켯서
+        self.currentStatus = '_' * len(self.secretWord)
 
     def display(self):
-        print(self.currentStatus)
-        print(self.numTries)
-
+        print("Current:", self.currentStatus)
+        print("Tries:", self.numTries)
 
     def guess(self, character):
-        #self.currentStatus = '_'*len(self.word) # word 길이 만큼 일단 전부 '_' 하기
+        #self.currentStatus = '_'*len(self.secretWord) # word 길이 만큼 일단 전부 '_' 하기
         self.guessedChar = character # 사용자가 입력한 추측한 단어
         #self.guessedChars.append[self.guessedChar] # 입력 단어를 저 위에 리스트에 추가
-        if self.guessedChar in self.secretWord:# 입력한 단어가 시크릿단어에 있으면
-            self.currentStatus += self.guessedChar
-            print(self.guessedChar, end ='')
-            if self.currentStatus == self.secretWord: # 현재상태랑 시크릿단어랑 똑같을 경우
-                return True
-            else:
-                return False
+        for i in range(len(self.secretWord)):
+            if self.secretWord[i] in self.guessedChar:# 입력한 단어가 시크릿단어에 있으면
+                self.currentStatus = self.currentStatus[:i] + self.secretWord[i] + self.currentStatus[i+1:]
+                if self.currentStatus == self.secretWord: # 현재상태랑 시크릿단어랑 똑같을 경우
+                    return True
+                else:
+                    return False
         else: # 실패시
             self.numTries += 1
