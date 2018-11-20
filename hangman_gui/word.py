c8b33b0e71af7488a884b85dacc299698d367fa9
leesjin <leesjin@kookmin.ac.kr>
2018-11-20 23:11:40 +0900

    hangman gui

---
+++ hangman_gui/word.py
@@ -0,0 +1,36 @@
+import random
+
+class Word:
+
+    def __init__(self, filename):
+        self.words = []
+        f = open(filename, 'r')
+        lines = f.readlines()
+        f.close()
+
+        self.count = 0
+        for line in lines:
+            word = line.rstrip()
+            self.words.append(word)
+            self.count += 1
+
+        print('%d words in DB' % self.count)
+
+
+    def test(self):
+        return 'default'
+
+
+    def randFromDB(self, minLength):
+        selected = False
+        while not selected:
+            r = random.randrange(self.count)
+            randstr = self.words[r]
+            if len(randstr) >= minLength:
+                return randstr
+
+'''
+if __name__ == "__main__":
+    word = Word("words.txt")
+    print(word.randFromDB(13))
+'''
